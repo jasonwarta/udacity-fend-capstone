dff2b44dea21d0ee45222ac5adcab12269c84141
Jason Warta <jasonwarta@gmail.com>
2020-10-16 01:00:39 -0800

    added first test

---
+++ __test__/formHandler.test.js
@@ -0,0 +1,14 @@
+import {handleSubmit} from '../src/client/js/handleSubmit'
+
+// more in-depth testing for this sort of function isn't practical
+// it requires a simulated event, a running server, and costs API credits
+
+describe("Testing handleSubmit", () => {
+    test("Function is defined", () => {
+        expect(handleSubmit).toBeDefined()
+    })
+
+    test("Function throws on undefined event", () => {
+        expect(handleSubmit).toThrowError(TypeError)
+    })
+})
